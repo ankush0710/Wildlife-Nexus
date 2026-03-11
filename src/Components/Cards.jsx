b1bf9cb203726eaa6a44edc40e70a9c9e2e1cd28
ankush0710 <ankushkurvey053@gmail.com>
2026-03-11 17:41:16 +0530

    reusable card for wildlife is created

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,35 +1,13 @@
-// {/* <div id="program-card" className="relative h-auto mx-5 rounded-xl flex flex-col gap-5 items-center bg-cover bg-center md:bg-none" style={{backgroundImage: `url(${sample})`}}>
-//         {/* <div id="card-image" className="relative w-78 h-78 border border-gray-500 mt-10 ">
-//           <img src={sample} alt="image" className="w-full h-full object-contain"/>
-//         </div> */}
-//         <div className="absolute inset-0 bg-black opacity-25"></div>
-        
-//         <div id="description" className="relative px-5 py-10 space-y-3">
-//           <h1 className="font-heading font-bold text-xl text-white">Project Tiger</h1>
-//           <p className="font-body text-white">
-//             Project Tiger was launched in 1973 with the aim of conserving Bengal
-//             tigers. This program established over 50 tiger reserves across India
-//             to safeguard tigers and their natural habitats. Efforts include
-//             anti-poaching strategies, habitat restoration, and enhancing the
-//             protected areas' management. Additionally, eco-tourism is promoted
-//             around tiger reserves to generate funds and engage local
-//             communities. The initiative has seen a significant increase in tiger
-//             populations in India and has played a vital role in creating a
-//             broader conservation model.
-//           </p>
-//         </div>
-//         <div className="relative pb-10">
-//           <button
-//             type="submit"
-//             className="text-[#8A7650] font-semibold font-body bg-transparent border-2 border-[#8A7650] px-6 py-2 rounded-full group hover:text-[#562F00] hover:bg-[#8A7650] hover:border-2 hover:border-[#562F00] hover:duration-600"
-//           >
-//             view article
-//             <span>
-//               <FontAwesomeIcon
-//                 icon="fa-solid fa-arrow-down"
-//                 className="ps-5 group-hover:translate-y-1 transition duration-300"
-//               />
-//             </span>
-//           </button>
-//         </div>
-//     </div> */}
+import React from "react";
+
+const Cards = ({children}) => {
+  return (
+    <>
+      <div id="card" className="relative max-w-md h-86 border-2 border-black mx-3 rounded-xl group md:shrink-0 md:max-w-xl md:h-90">
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Cards;
--- src/Pages/Wildlife.jsx
+++ src/Pages/Wildlife.jsx
@@ -1,14 +1,59 @@
-import React from 'react'
+import React from "react";
+import Cards from "../Components/Cards";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import sample from "../assets/sample-image.jpg"
 
 const Wildlife = () => {
+  return (
+    <>
+      <div className="w-full h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <Cards>
+          {/* background image div */}
+          <div
+            style={{ backgroundImage: `url('${sample}')` }}
+            className="absolute w-full h-full bg-cover bg-center rounded-lg"
+          >
+            {/* overlay div  */}
+            <div className="absolute inset-0 bg-black opacity-50 rounded-lg"></div>
+          </div>
+          <div className="relative z-10 p-6 space-y-1 group-hover:text-hidden">
+            <h1 className="text-white font-heading text-xl">
+              Name : <span className="font-body text-lg ps-5">Parrot</span>
+            </h1>
+            <h1 className="text-white font-heading text-xl">
+              Species : <span className="font-body text-lg ps-5">Loxodon</span>
+            </h1>
+            <h1 className="text-white font-heading text-xl">
+              Habitat : <span className="font-body text-lg ps-5">Savana</span>
+            </h1>
+            <h1 className="text-white font-heading text-xl">
+              Diet : <span className="font-body text-lg ps-5">Herbivorous</span>
+            </h1>
+            <h1 className="text-white font-heading text-xl">
+              Status :{" "}
+              <span className="font-body text-lg ps-5">Vulnerable</span>
+            </h1>
+            <h1 className="text-white font-heading text-xl">
+              LifeSpan (years) :{" "}
+              <span className="font-body text-lg ps-5">10</span>
+            </h1>
+            <h1 className="text-white font-heading text-xl">
+              Weight (in kgs) :{" "}
+              <span className="font-body text-lg ps-5">6000</span>
+            </h1>
+            <h1 className="text-white font-heading text-xl">
+              Height (in meters) :{" "}
+              <span className="font-body text-lg ps-5">300</span>
+            </h1>
+            <h1 className="text-white font-heading text-xl">
+              Speed (in kms) :{" "}
+              <span className="font-body text-lg ps-5">40</span>
+            </h1>
+          </div>
+        </Cards>
+      </div>
+    </>
+  );
+};
 
-    return(
-        <>
-        <div>
-            This is WildLife Page
-        </div>
-        </>
-    )
-}
-
-export default Wildlife
+export default Wildlife;
