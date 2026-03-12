279d571dc7399a7fb3be09030604351d23bf307b
ankush0710 <ankushkurvey053@gmail.com>
2026-03-12 12:20:31 +0530

    pagination added to wildlife component

--- src/Pages/Wildlife.jsx
+++ src/Pages/Wildlife.jsx
@@ -14,11 +14,35 @@ const Wildlife = () => {
     dispatch(FetchWildlifeData());
   }, [WildlifeData]);
 
+  //method for displaying 8 cards in md devices and 9 cards in lg devices
+  useEffect(()=>{
+    if(window.innerWidth >= 1024){
+      setCardPerPage(9);
+    }
+    else if(window.innerWidth >= 768){
+      setCardPerPage(8);
+    }
+  })
 
   //pagination logic start here
-  const pagination = useMemo(()=>{
-    
-  }, [])
+  const currentCard = useMemo(() => {
+    const lastIndex = currentPage * cardPerPage;
+    const startIndex = lastIndex - cardPerPage;
+    return WildlifeData.slice(startIndex, lastIndex);
+  }, [currentPage, cardPerPage]);
+
+  // total number of pages will display 
+  const TotalPage = Math.ceil(WildlifeData.length / cardPerPage);
+
+  const selectPageHandle = (selected) => {
+    if (
+      selected >= 1 &&
+      selected !== currentPage &&
+      selected <= TotalPage
+    ) {
+      setCurrentPage(selected);
+    }
+  };
 
   //if data is yet to come then loading text will display
   if (!WildlifeData || WildlifeData.length == 0) {
@@ -32,8 +56,8 @@ const Wildlife = () => {
   }
   return (
     <>
-      <div className="w-full h-auto my-10 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-        {WildlifeData.map((Wdata) => {
+      <div className="w-full h-auto my-10 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {currentCard.map((Wdata) => {
           return (
             <Cards key={Wdata.id}>
               {/* background image div */}
@@ -46,38 +70,45 @@ const Wildlife = () => {
               </div>
               <div className="relative z-10 p-6 space-y-1 group-hover:text-hidden">
                 <h1 className="text-white font-heading text-xl">
-                  Name : <span className="font-body text-lg ps-5">{Wdata.name}</span>
+                  Name :{" "}
+                  <span className="font-body text-lg ps-5">{Wdata.name}</span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
-                  Species :{" "}
-                  <span className="font-body text-lg ps-5">{Wdata.species}</span>
+                  Species :
+                  <span className="font-body text-lg ps-5">
+                    {Wdata.species}
+                  </span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
-                  Habitat :{" "}
-                  <span className="font-body text-lg ps-5">{Wdata.habitat}</span>
+                  Habitat :
+                  <span className="font-body text-lg ps-5">
+                    {Wdata.habitat}
+                  </span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
-                  Diet :{" "}
+                  Diet :
                   <span className="font-body text-lg ps-5">{Wdata.diet}</span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
-                  Status :{" "}
+                  Status :
                   <span className="font-body text-lg ps-5">{Wdata.status}</span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
-                  LifeSpan (years) :{" "}
-                  <span className="font-body text-lg ps-5">{Wdata.lifespan}</span>
+                  LifeSpan (years) :
+                  <span className="font-body text-lg ps-5">
+                    {Wdata.lifespan}
+                  </span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
-                  Weight (in kgs) :{" "}
+                  Weight (in kgs) :
                   <span className="font-body text-lg ps-5">{Wdata.weight}</span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
-                  Height (in meters) :{" "}
+                  Height (in meters) :
                   <span className="font-body text-lg ps-5">{Wdata.height}</span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
-                  Speed (in kms) :{" "}
+                  Speed (in kms) :
                   <span className="font-body text-lg ps-5">{Wdata.speed}</span>
                 </h1>
               </div>
@@ -85,6 +116,50 @@ const Wildlife = () => {
           );
         })}
       </div>
+      {WildlifeData.length > 0 && (
+        <div
+          id="pagination"
+          className="flex justify-center text-xl mt-5 space-x-5 font-heading font-semibold mb-10"
+        >
+          <span>
+            <FontAwesomeIcon
+              icon="fa-solid fa-angle-left"
+              className={
+                currentPage > 1
+                  ? "text-gray-400 hover:text-black hover:transform hover:scale-130 hover:duration-300 cursor-pointer"
+                  : "opacity-0"
+              }
+              onClick={() => selectPageHandle(currentPage - 1)}
+            />
+          </span>
+          {[...Array.from({length: TotalPage})].map((_, i) => {
+            return (
+              <span
+                key={i}
+                className={
+                  currentPage == i + 1
+                    ? "text-black transform translate scale-140 ease-in-out duration-300 cursor-pointer"
+                    : "text-gray-400 cursor-pointer"
+                }
+                onClick={() => selectPageHandle(i + 1)}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span>
+            <FontAwesomeIcon
+              icon="fa-solid fa-angle-right"
+              className={
+                currentPage !== TotalPage
+                  ? "text-gray-400 hover:text-black hover:transform hover:scale-130 hover:duration-300 cursor-pointer"
+                  : "opacity-0"
+              }
+              onClick={() => selectPageHandle(currentPage + 1)}
+            />
+          </span>
+        </div>
+      )}
     </>
   );
 };
