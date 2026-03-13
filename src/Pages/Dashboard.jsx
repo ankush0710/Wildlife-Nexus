620d2fc80bd34edc9a68d92c1a745067fef9b2a4
ankush0710 <ankushkurvey053@gmail.com>
2026-03-13 15:49:51 +0530

    opacity  added to tha navbar

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import bannerVideo from "../assets/bannerVideo.mp4";
 
 const Dashboard = () => {
-    return(
-        <h1>This is Dashboard page</h1>
-    )
-}
+  return (
+    <>
+      <div id="video-container" className="w-full min-h-screen absolute inset-0 overflow-hidden">
+        <video autoPlay muted loop className="w-contain h-screen absolute">
+          <source src={bannerVideo} type="video/mp4" />
+        </video>
+        <div className="bg-black absolute inset-0 opacity-50"></div>
+        <h1 className="relative z-10 text-3xl text-[#FFF8F0] font-heading font-bold px-10">
+          Home
+        </h1>
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
