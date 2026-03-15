e3829c615fde02cf4549ae0d806fcb6becaaf15f
ankush0710 <ankushkurvey053@gmail.com>
2026-03-15 14:38:27 +0530

    banner image added to join our team page

--- src/Pages/JoinTeam.jsx
+++ src/Pages/JoinTeam.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import bgImage from "../assets/Giant-panda.webp";
 
 const JoinTeam = () => {
-    return(
-        <>
-        <h1>This is Join Team</h1>
-        </>
-    )
-}
+  return (
+    <>
+    {/* bg -image and intro of page  */}
+      <div id="bg-image" className="w-full min-h-screen">
+        <div
+          id="banner-image"
+          style={{ backgroundImage: `url('${bgImage}')` }}
+          className="absolute top-0 w-full min-h-screen z-0 overflow-hidden bg-no-repeat bg-cover bg-center flex items-center justify-center"
+        >
+          <div className="bg-black absolute inset-0 opacity-50"></div>
+          <div className="absolute z-10 inset-0 top-1/2 space-y-4">
+            <h1 className="text-4xl text-center text-[#44A194] font-heading font-bold md:text-7xl">
+              Join Our Team
+            </h1>
+          </div>
+        </div>
+      </div>
 
-export default JoinTeam
+      {/* carausal section for goverment initiatives and Program  */}
+      <div id="gov-carousal">
+        
+      </div>
+    </>
+  );
+};
+
+export default JoinTeam;
+
+// https://sheet2api.com/v1/LZ3hk9ALR3i2/goverment_initiatives
