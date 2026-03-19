a6d823bbba6304c55c03eed671b77e0b2e3da599
ankush0710 <ankushkurvey053@gmail.com>
2026-03-19 18:09:16 +0530

    some changes done

--- src/Pages/Wildlife.jsx
+++ src/Pages/Wildlife.jsx
@@ -87,216 +87,238 @@ const Wildlife = () => {
       {/* bg -image and intro of page  */}
       <section id="bg-image">
         <div className="w-full min-h-[80vh]">
-        <div
-          id="banner-image"
-          style={{ backgroundImage: `url('${WildlifeBgImage}')` }}
-          className="absolute top-0 w-full min-h-screen z-0 overflow-hidden bg-no-repeat bg-cover bg-center flex items-center justify-center"
-        >
-          <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-black/20"></div>
-          <div className="absolute z-10 inset-0 top-1/2 space-y-4 lg:px-80">
-            <h1 className="text-2xl text-center text-[#EFD2B0] font-heading font-bold">
-              <FontAwesomeIcon
-                icon="fa-solid fa-quote-left"
-                className="me-2 mb-1"
-              />
-              Making a difference to the welfare of Animals doesn't require a
-              massive effort; it requires small actions that can make a
-              significant impact.
-              <FontAwesomeIcon
-                icon="fa-solid fa-quote-right"
-                className="ms-2 mb-1"
-              />
-            </h1>
+          <div
+            id="banner-image"
+            style={{ backgroundImage: `url('${WildlifeBgImage}')` }}
+            className="absolute top-0 w-full min-h-screen z-0 overflow-hidden bg-no-repeat bg-cover bg-center flex items-center justify-center"
+          >
+            <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-black/20"></div>
+            <div className="absolute z-10 inset-0 top-1/2 space-y-4 lg:px-80">
+              <h1 className="text-2xl text-center text-[#EFD2B0] font-heading font-bold">
+                <FontAwesomeIcon
+                  icon="fa-solid fa-quote-left"
+                  className="me-2 mb-1"
+                />
+                Making a difference to the welfare of Animals doesn't require a
+                massive effort; it requires small actions that can make a
+                significant impact.
+                <FontAwesomeIcon
+                  icon="fa-solid fa-quote-right"
+                  className="ms-2 mb-1"
+                />
+              </h1>
+            </div>
           </div>
         </div>
-      </div>
       </section>
 
       {/* button for back to default state  */}
-      <div className="mx-3 my-2 md:my-10 flex justify-between items-end md:flex-row md:justify-between md:items-center md:mx-15">
-        {(search || catagory) && (
-          <button
-            onClick={handleBack}
-            className="flex justify-center items-center gap-2 cursor-pointer"
-          >
-            <FontAwesomeIcon
-              icon="fa-solid fa-hand-point-left"
-              className="text-2xl text-[#44A194]"
-            />
-            <span className="font-heading font-semibold text-2xl text-[#44A194]">
-              Back
-            </span>
-          </button>
-        )}
-        
-        {/* //search bar and filter button */}
-        <div className={(search || catagory) ? "flex flex-col gap-5 items-center justify-between md:flex-1 md:flex-row" : "flex flex-1 justify-center items-center"}>
-          <div className="relative mx-auto">
-            <input
-              type="text"
-              value={filteredSearch}
-              placeholder="Search by name"
-              className="py-2 px-5 border border-gray-500 rounded-full bg-gray-300 focus:border-gray-600 md:px-8 lg:px-10"
-              onChange={(e) => setFilteredSearch(e.target.value)}
-            />
+      <section id="searchBar-section">
+        <div className="mx-3 my-2 md:my-10 flex justify-between items-end md:flex-row md:justify-between md:items-center md:mx-15">
+          {(search || catagory) && (
             <button
-              onClick={() => {
-                setSearch(filteredSearch);
-                setCurrentPage(1);
-              }}
-              className="absolute inset-y-0 right-0 px-4 rounded-e-full border border-gray-500 bg-blue-500 text-white font-body font-semibold hover:bg-[#111F35] cursor-pointer"
+              onClick={handleBack}
+              className="flex justify-center items-center gap-2 cursor-pointer"
             >
-              Search
-            </button>
-          </div>
-
-          {/* dropdown for filter the cards by species  */}
-          <div className="relative flex flex-col justify-center items-center">
-            <div
-              onClick={() => setIsHidden(!isHidden)}
-              className="bg-gray-300 flex items-center gap-8 px-5 py-2 border border-gray-500 cursor-pointer"
-            >
-              <span className="text-xl font-body font-bold text-gray-700">
-                {catagory ? catagory : "Species"}
-              </span>
               <FontAwesomeIcon
-                icon="fa-solid fa-angle-down"
-                className={
-                  isHidden
-                    ? "text-xl text-gray-700 transition transform rotate-0 ease-in-out duration-300"
-                    : "text-xl text-gray-700 transition transform rotate-180 ease-in-out duration-300 "
-                }
+                icon="fa-solid fa-hand-point-left"
+                className="text-2xl text-[#44A194]"
               />
+              <span className="font-heading font-semibold text-2xl text-[#44A194]">
+                Back
+              </span>
+            </button>
+          )}
+
+          {/* //search bar and filter button */}
+          <div
+            className={
+              search || catagory
+                ? "flex flex-col gap-5 items-center justify-between md:flex-1 md:flex-row"
+                : "flex flex-1 justify-center items-center"
+            }
+          >
+            <div className="relative mx-auto">
+              <input
+                type="text"
+                value={filteredSearch}
+                placeholder="Search by name"
+                className="py-2 px-5 border border-gray-500 rounded-full bg-gray-300 focus:border-gray-600 md:px-8 lg:px-10"
+                onChange={(e) => setFilteredSearch(e.target.value)}
+              />
+              <button
+                onClick={() => {
+                  setSearch(filteredSearch);
+                  setCurrentPage(1);
+                }}
+                className="absolute inset-y-0 right-0 px-4 rounded-e-full border border-gray-500 bg-blue-500 text-white font-body font-semibold hover:bg-[#111F35] cursor-pointer"
+              >
+                Search
+              </button>
             </div>
 
-            {/* dropdown list  */}
-            <div
-              className={
-                isHidden
-                  ? "hidden"
-                  : "border-2 border-gray-300 top-12 shadow-xl absolute z-30"
-              }
-            >
-              <p
-                onClick={() => {
-                  setCatagory("");
-                  setIsHidden(true);
-                }}
-                className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
+            {/* dropdown for filter the cards by species  */}
+            <div className="relative flex flex-col justify-center items-center">
+              <div
+                onClick={() => setIsHidden(!isHidden)}
+                className="bg-gray-300 flex items-center gap-8 px-5 py-2 border border-gray-500 cursor-pointer"
               >
-                All
-              </p>
-              <p
-                onClick={() => {
-                  setCatagory("Endangered");
-                  setIsHidden(true);
-                  setCurrentPage(1);
-                }}
-                className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
+                <span className="text-xl font-body font-bold text-gray-700">
+                  {catagory ? catagory : "Species"}
+                </span>
+                <FontAwesomeIcon
+                  icon="fa-solid fa-angle-down"
+                  className={
+                    isHidden
+                      ? "text-xl text-gray-700 transition transform rotate-0 ease-in-out duration-300"
+                      : "text-xl text-gray-700 transition transform rotate-180 ease-in-out duration-300 "
+                  }
+                />
+              </div>
+
+              {/* dropdown list  */}
+              <div
+                className={
+                  isHidden
+                    ? "hidden"
+                    : "border-2 border-gray-300 top-12 shadow-xl absolute z-30"
+                }
               >
-                Endangered
-              </p>
-              <p
-                onClick={() => {
-                  setCatagory("Vulnerable");
-                  setIsHidden(true);
-                  setCurrentPage(1);
-                }}
-                className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
-              >
-                Vulnerable
-              </p>
-              <p
-                onClick={() => {
-                  setCatagory("Least Concern");
-                  setIsHidden(true);
-                  setCurrentPage(1);
-                }}
-                className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
-              >
-                Least Concern
-              </p>
-              <p
-                onClick={() => {
-                  setCatagory("Near Threatened");
-                  setIsHidden(true);
-                  setCurrentPage(1);
-                }}
-                className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
-              >
-                Near Threatened
-              </p>
+                <p
+                  onClick={() => {
+                    setCatagory("");
+                    setIsHidden(true);
+                  }}
+                  className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
+                >
+                  All
+                </p>
+                <p
+                  onClick={() => {
+                    setCatagory("Endangered");
+                    setIsHidden(true);
+                    setCurrentPage(1);
+                  }}
+                  className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
+                >
+                  Endangered
+                </p>
+                <p
+                  onClick={() => {
+                    setCatagory("Vulnerable");
+                    setIsHidden(true);
+                    setCurrentPage(1);
+                  }}
+                  className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
+                >
+                  Vulnerable
+                </p>
+                <p
+                  onClick={() => {
+                    setCatagory("Least Concern");
+                    setIsHidden(true);
+                    setCurrentPage(1);
+                  }}
+                  className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
+                >
+                  Least Concern
+                </p>
+                <p
+                  onClick={() => {
+                    setCatagory("Near Threatened");
+                    setIsHidden(true);
+                    setCurrentPage(1);
+                  }}
+                  className="text-lg font-body font-semibold bg-white border-1 border-gray-300 text-center text-nowrap text-gray-800 cursor-pointer py-2 px-13 hover:bg-gray-400"
+                >
+                  Near Threatened
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
 
       {/* main content section  */}
-      <div className="px-5 w-full h-auto my-10 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:px-10">
-        {currentCard.map((Wdata) => {
-          return (
-            <Cards
-              key={Wdata.id}
-              className="transform transition-all duration-300 ease-in-out hover:scale-120 hover:shadow-3xl hover:z-10"
-            >
-              {/* background image div */}
-              <div
-                style={{ backgroundImage: `url('${Wdata.imageUrl}')` }}
-                className="absolute w-full h-full bg-cover bg-center rounded-lg"
+      <section id="main-section">
+        <div className="px-5 w-full h-auto my-10 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:px-10">
+          {currentCard.map((Wdata) => {
+            return (
+              <Cards
+                key={Wdata.id}
+                className="transform transition-all duration-300 ease-in-out hover:scale-120 hover:shadow-3xl hover:z-10"
               >
-                {/* overlay div  */}
-                <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-black/20 rounded-lg"></div>
-              </div>
-              <div className="relative z-10 p-6 space-y-1 group-hover:text-hidden">
-                <h1 className="text-white font-heading text-xl">
-                  Name :
-                  <span className="font-body text-lg ps-5">{Wdata.name}</span>
-                </h1>
-                <h1 className="text-white font-heading text-xl">
-                  Species :
-                  <span className="font-body text-lg ps-5">
-                    {Wdata.species}
-                  </span>
-                </h1>
-                <h1 className="text-white font-heading text-xl">
-                  Habitat :
-                  <span className="font-body text-lg ps-5">
-                    {Wdata.habitat}
-                  </span>
-                </h1>
-                <h1 className="text-white font-heading text-xl">
-                  Diet :
-                  <span className="font-body text-lg ps-5">{Wdata.diet}</span>
-                </h1>
-                <h1 className="text-white font-heading text-xl">
-                  Status :
-                  <span className="font-body text-lg ps-5">{Wdata.status}</span>
-                </h1>
-                <h1 className="text-white font-heading text-xl">
-                  LifeSpan (years) :
-                  <span className="font-body text-lg ps-5">
-                    {Wdata.lifespan}
-                  </span>
-                </h1>
-                <h1 className="text-white font-heading text-xl">
-                  Weight (in kgs) :
-                  <span className="font-body text-lg ps-5">{Wdata.weight}</span>
-                </h1>
-                <h1 className="text-white font-heading text-xl">
-                  Height (in meters) :
-                  <span className="font-body text-lg ps-5">{Wdata.height}</span>
-                </h1>
-                <h1 className="text-white font-heading text-xl">
-                  Speed (in KM) :
-                  <span className="font-body text-lg ps-5">{Wdata.speed}</span>
-                </h1>
-              </div>
-            </Cards>
-          );
-        })}
-      </div>
-    
-      <Pagination TotalPage={TotalPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                {/* background image div */}
+                <div
+                  style={{ backgroundImage: `url('${Wdata.imageUrl}')` }}
+                  className="absolute w-full h-full bg-cover bg-center rounded-lg"
+                >
+                  {/* overlay div  */}
+                  <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-black/20 rounded-lg"></div>
+                </div>
+                <div className="relative z-10 p-6 space-y-1 group-hover:text-hidden">
+                  <h1 className="text-white font-heading text-xl">
+                    Name :
+                    <span className="font-body text-lg ps-5">{Wdata.name}</span>
+                  </h1>
+                  <h1 className="text-white font-heading text-xl">
+                    Species :
+                    <span className="font-body text-lg ps-5">
+                      {Wdata.species}
+                    </span>
+                  </h1>
+                  <h1 className="text-white font-heading text-xl">
+                    Habitat :
+                    <span className="font-body text-lg ps-5">
+                      {Wdata.habitat}
+                    </span>
+                  </h1>
+                  <h1 className="text-white font-heading text-xl">
+                    Diet :
+                    <span className="font-body text-lg ps-5">{Wdata.diet}</span>
+                  </h1>
+                  <h1 className="text-white font-heading text-xl">
+                    Status :
+                    <span className="font-body text-lg ps-5">
+                      {Wdata.status}
+                    </span>
+                  </h1>
+                  <h1 className="text-white font-heading text-xl">
+                    LifeSpan (years) :
+                    <span className="font-body text-lg ps-5">
+                      {Wdata.lifespan}
+                    </span>
+                  </h1>
+                  <h1 className="text-white font-heading text-xl">
+                    Weight (in kgs) :
+                    <span className="font-body text-lg ps-5">
+                      {Wdata.weight}
+                    </span>
+                  </h1>
+                  <h1 className="text-white font-heading text-xl">
+                    Height (in meters) :
+                    <span className="font-body text-lg ps-5">
+                      {Wdata.height}
+                    </span>
+                  </h1>
+                  <h1 className="text-white font-heading text-xl">
+                    Speed (in KM) :
+                    <span className="font-body text-lg ps-5">
+                      {Wdata.speed}
+                    </span>
+                  </h1>
+                </div>
+              </Cards>
+            );
+          })}
+        </div>
+      </section>
+
+      <Pagination
+        TotalPage={TotalPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </>
   );
 };
